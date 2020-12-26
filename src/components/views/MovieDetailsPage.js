b7d9de0c5773f5a7277be308b707ca159cca5f4a
Roman Palna <roma.palna@gmail.com>
2020-12-26 20:40:05 +0200

    Realize film details

--- src/components/views/MovieDetailsPage.js
+++ src/components/views/MovieDetailsPage.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import fatchFilmDetail from '../../FilmAPI/filmDetailApi';
+import Loader from './loader';
+import s from './ViewsStyles.module.css';
 
 export default function MovieDetailsPage() {
   const [film, setFilm] = useState(null);
@@ -13,19 +15,27 @@ export default function MovieDetailsPage() {
 
   console.log(film);
 
-  return (
-    <div>
+  return film ? (
+    <div className={s.detailsPage}>
       <img
+        className={s.detailsImg}
         alt={film.title}
         src={`https://image.tmdb.org/t/p/w400${film.poster_path}`}
       ></img>
-      <div>
+      <div className={s.detailsText}>
         <h1>{film.title}</h1>
-
         <p>Vote Average: {film.vote_average} / 10</p>
         <h2>Overview</h2>
         <p>{film.overview}</p>
+        <h2>Ganres:</h2>
+        <ul>
+          {film.genres.map(ganre => (
+            <li key={ganre.id}>{ganre.name}</li>
+          ))}
+        </ul>
       </div>
     </div>
+  ) : (
+    <Loader />
   );
 }
